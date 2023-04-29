1112ccd57a9bebbc69baec6ae587aaca09bf538d
DavidMerino25 <deividrocan2508@gmail.com>
2023-04-29 09:39:58 -0600

    learn how to write text in web elements

--- app/src/test/java/pages/GooglePage.java
+++ app/src/test/java/pages/GooglePage.java
@@ -2,8 +2,8 @@ package pages;
 
 public class GooglePage extends BasePages{ //heredamos de la clase BasePages
 
-    private String searchButton = "//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[4]/center[1]/input[1]";//variable de tipo String que contiene el xpath del elemento web
-
+    private String searchButton = "//input[@value='Buscar con Google']";//variable de tipo String que contiene el xpath del elemento web
+    private String searchTextField ="//textarea[@id='APjFqb']"; //variable de tipo String que contiene el xpath del elemento web para buscar
     public GooglePage(){ //constructor
         super(driver);
     }
@@ -14,6 +14,10 @@ public class GooglePage extends BasePages{ //heredamos de la clase BasePages
 
     public void clickGoogleSearch(){ //metodo para hacer click en el boton de busqueda
         clickElement(searchButton); //llamamos al metodo clickElement de la clase BasePages
+    }
+
+    public void writeSearchCriteria(String searchCriteria){ //metodo para escribir el criterio de busqueda
+        writeText(searchTextField, searchCriteria); //llamamos al metodo writeText de la clase BasePages
     }
 
 
--- app/src/test/java/steps/GoogleSteps.java
+++ app/src/test/java/steps/GoogleSteps.java
@@ -10,16 +10,16 @@ public class GoogleSteps {
    @Given("^I am on the Google search page$")
    public void navigateToGoogle() {
       googlePage.navigateToGoogle(); //llamamos al metodo navigateToGoogle de la clase GooglePage
-      googlePage.clickGoogleSearch(); //llamamos al metodo clickGoogleSearch de la clase GooglePage
+      
    }
 
    @When("^I enter a search criteria")
    public void enterSearchCriteria() {
-      
+      googlePage.writeSearchCriteria("Selenium"); //llamamos al metodo writeSearchCriteria de la clase GooglePage
    }
    @And("^Click on the search button$")
    public void clickSearchButton() {
-      
+      googlePage.clickGoogleSearch(); //llamamos al metodo clickGoogleSearch de la clase GooglePage
    }
 
    @Then("^The result matches the search criteria$")
